06e8a41856bacb2f0ba7c67bcff3bef9a5401279
vagrant <vagrant@homestead>
2021-03-18 02:09:32 +0000

    fix company display

--- app/Admin/Controllers/CompanyController.php
+++ app/Admin/Controllers/CompanyController.php
@@ -31,14 +31,21 @@ class CompanyController extends AdminController
         $grid->column('logo_path', __('公司 Logo'))->image();
         $grid->column('contact', __('联系方式'));
         $grid->column('address', __('地址'));
-        $grid->column('description', __('描述信息'));
+        $grid->column('description', __('描述信息'))->width(300);
         $grid->column('created_at', __('创建时间'));
         $grid->column('updated_at', __('更新时间'));
 
         $grid->actions(function ($actions) {
-            // 去掉删除
             $actions->disableDelete();
         });
+
+        $grid->disableExport();
+        $grid->disableCreation();
+        $grid->disableRowSelector();
+        $grid->disableFilter();
+//        $grid->filter(function ($filter) {
+//            $filter->disableIdFilter(); // 去掉默认的 id 过滤器
+//        });
 
         return $grid;
     }
@@ -58,9 +65,13 @@ class CompanyController extends AdminController
         $show->field('logo_path', __('公司 Logo'))->image();
         $show->field('contact', __('联系方式'));
         $show->field('address', __('地址'));
-        $show->field('description', __('描述信息'))->editor();
+        $show->field('description', __('描述信息'))->unescape();
         $show->field('created_at', __('创建时间'));
         $show->field('updated_at', __('更新时间'));
+
+        $show->panel()->tools(function ($tools) {
+            $tools->disableDelete();
+        });
 
         return $show;
     }
@@ -80,6 +91,10 @@ class CompanyController extends AdminController
         $form->text('contact', __('联系方式'))->rules('required');
         $form->text('address', __('地址'))->rules('required');
         $form->UEditor('description', __('描述信息'))->rules('required');
+
+        $form->tools(function (Form\Tools $tools) {
+            $tools->disableDelete();
+        });
 
         return $form;
     }
--- config/admin.php
+++ config/admin.php
@@ -11,7 +11,7 @@ return [
     | login page.
     |
     */
-    'name' => 'Laravel-admin',
+    'name' => 'Purifier-Mall',
 
     /*
     |--------------------------------------------------------------------------
@@ -22,7 +22,7 @@ return [
     | `img` tag, eg '<img src="http://logo-url" alt="Admin logo">'.
     |
     */
-    'logo' => '<b>Laravel</b> admin',
+    'logo' => '<b>Purifier-Mall</b> admin',
 
     /*
     |--------------------------------------------------------------------------
@@ -34,7 +34,7 @@ return [
     | '<img src="http://logo-url" alt="Admin logo">'.
     |
     */
-    'logo-mini' => '<b>La</b>',
+    'logo-mini' => '<b>Pu</b>',
 
     /*
     |--------------------------------------------------------------------------
@@ -85,7 +85,7 @@ return [
     | Html title for all pages.
     |
     */
-    'title' => 'Admin',
+    'title' => 'Purifier-Mall',
 
     /*
     |--------------------------------------------------------------------------
