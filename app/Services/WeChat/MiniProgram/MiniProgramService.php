9698943fedc22cce03fdb1b11ba2de94e1a62926
vagrant <vagrant@homestead>
2021-03-18 15:57:31 +0000

    add miniprogram decrypt data

--- app/Services/WeChat/MiniProgram/MiniProgramService.php
+++ app/Services/WeChat/MiniProgram/MiniProgramService.php
@@ -3,8 +3,21 @@
 namespace App\Services\WeChat\MiniProgram;
 
 use Illuminate\Http\Request;
+use App\Services\WeChat\WeChatService;
 
 class MiniProgramService
 {
+    private $miniProgram;
 
+    public function __construct()
+    {
+        $this->miniProgram = WeChatService::miniProgram();
+    }
+
+    public function decryptData(string $code, string $iv, string $encryptedData): string
+    {
+        $session = $this->miniProgram->auth->session($code);
+
+        return $this->miniProgram->encryptor->decryptData($session, $iv, $encryptedData);
+    }
 }
