6f48ee7d9fc1421016f43bdc3ff371d61d78c469
vagrant <vagrant@homestead>
2021-04-02 08:29:32 +0000

    add seeder

---
+++ database/seeds/AdminTablesSeeder.php
@@ -0,0 +1,201 @@
+<?php
+
+namespace Database\Seeders;
+
+use DB;
+use Illuminate\Database\Seeder;
+
+class AdminTablesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // base tables
+        \Encore\Admin\Auth\Database\Menu::truncate();
+        \Encore\Admin\Auth\Database\Menu::insert(
+            [
+                [
+                    "parent_id" => 0,
+                    "order" => 1,
+                    "title" => "仪表盘",
+                    "icon" => "fa-bar-chart",
+                    "uri" => "/",
+                    "permission" => NULL
+                ],
+                [
+                    "parent_id" => 0,
+                    "order" => 2,
+                    "title" => "权限管理",
+                    "icon" => "fa-tasks",
+                    "uri" => NULL,
+                    "permission" => NULL
+                ],
+                [
+                    "parent_id" => 2,
+                    "order" => 3,
+                    "title" => "管理员",
+                    "icon" => "fa-users",
+                    "uri" => "auth/users",
+                    "permission" => NULL
+                ],
+                [
+                    "parent_id" => 2,
+                    "order" => 4,
+                    "title" => "角色",
+                    "icon" => "fa-user",
+                    "uri" => "auth/roles",
+                    "permission" => NULL
+                ],
+                [
+                    "parent_id" => 2,
+                    "order" => 5,
+                    "title" => "权限",
+                    "icon" => "fa-ban",
+                    "uri" => "auth/permissions",
+                    "permission" => NULL
+                ],
+                [
+                    "parent_id" => 2,
+                    "order" => 6,
+                    "title" => "菜单",
+                    "icon" => "fa-bars",
+                    "uri" => "auth/menu",
+                    "permission" => NULL
+                ],
+                [
+                    "parent_id" => 2,
+                    "order" => 7,
+                    "title" => "操作日志",
+                    "icon" => "fa-history",
+                    "uri" => "auth/logs",
+                    "permission" => NULL
+                ],
+                [
+                    "parent_id" => 0,
+                    "order" => 8,
+                    "title" => "轮播图管理",
+                    "icon" => "fa-image",
+                    "uri" => "slideshows",
+                    "permission" => NULL
+                ],
+                [
+                    "parent_id" => 0,
+                    "order" => 11,
+                    "title" => "公司管理",
+                    "icon" => "fa-compass",
+                    "uri" => "companies",
+                    "permission" => NULL
+                ],
+                [
+                    "parent_id" => 0,
+                    "order" => 12,
+                    "title" => "赚钱小技巧",
+                    "icon" => "fa-money",
+                    "uri" => "make-money-tips",
+                    "permission" => NULL
+                ],
+                [
+                    "parent_id" => 0,
+                    "order" => 9,
+                    "title" => "商品管理",
+                    "icon" => "fa-cubes",
+                    "uri" => "/products",
+                    "permission" => NULL
+                ],
+                [
+                    "parent_id" => 0,
+                    "order" => 10,
+                    "title" => "订单管理",
+                    "icon" => "fa-rmb",
+                    "uri" => "/orders",
+                    "permission" => NULL
+                ]
+            ]
+        );
+
+        \Encore\Admin\Auth\Database\Permission::truncate();
+        \Encore\Admin\Auth\Database\Permission::insert(
+            [
+                [
+                    "name" => "所有权限",
+                    "slug" => "*",
+                    "http_method" => "",
+                    "http_path" => "*"
+                ],
+                [
+                    "name" => "仪表盘",
+                    "slug" => "dashboard",
+                    "http_method" => "GET",
+                    "http_path" => "/"
+                ],
+                [
+                    "name" => "登录登出",
+                    "slug" => "auth.login",
+                    "http_method" => "",
+                    "http_path" => "/auth/login\r\n/auth/logout"
+                ],
+                [
+                    "name" => "用户设置",
+                    "slug" => "auth.setting",
+                    "http_method" => "GET,PUT",
+                    "http_path" => "/auth/setting"
+                ],
+                [
+                    "name" => "权限管理",
+                    "slug" => "auth.management",
+                    "http_method" => "",
+                    "http_path" => "/auth/roles\r\n/auth/permissions\r\n/auth/menu\r\n/auth/logs"
+                ],
+                [
+                    "name" => "商品管理",
+                    "slug" => "products",
+                    "http_method" => "",
+                    "http_path" => "/products*"
+                ],
+                [
+                    "name" => "订单管理",
+                    "slug" => "orders",
+                    "http_method" => "",
+                    "http_path" => "/orders*"
+                ]
+            ]
+        );
+
+        \Encore\Admin\Auth\Database\Role::truncate();
+        \Encore\Admin\Auth\Database\Role::insert(
+            [
+                [
+                    "name" => "超级管理员",
+                    "slug" => "超级管理员"
+                ]
+            ]
+        );
+
+        // pivot tables
+        DB::table('admin_role_menu')->truncate();
+        DB::table('admin_role_menu')->insert(
+            [
+                [
+                    "role_id" => 1,
+                    "menu_id" => 2
+                ]
+            ]
+        );
+
+        DB::table('admin_role_permissions')->truncate();
+        DB::table('admin_role_permissions')->insert(
+            [
+                [
+                    "role_id" => 1,
+                    "permission_id" => 1
+                ]
+            ]
+        );
+
+        // finish
+    }
+}
